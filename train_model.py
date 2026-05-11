cedca7a45c59af8da3c842879345908db081c949
Axel0252 <tafuro561@gmail.com>
2026-05-11 19:16:01 +0200

    Improved feature importance logic & Added RandomizedSearchCV logic

--- train_model.py
+++ train_model.py
@@ -16,15 +16,13 @@ from sklearn.metrics import (
     recall_score,
     roc_auc_score,
 )
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, train_test_split
 
 import misc
 
 # Configs
-DATASET_PATH = "dataset/"
+# DROP_COLUMNS = ["Timestamp", "Src IP", "Dst IP", "Flow ID"] not dropped in the latest experiments
 CLEAN_DATASET = "cic_ids2018_cleaned.csv"
-CHUNK_SIZE = 500000
-DROP_COLUMNS = ["Timestamp", "Src IP", "Dst IP", "Flow ID"]
 
 
 def load_and_prepare():
@@ -43,16 +41,19 @@ def load_and_prepare():
 
 
 def train_model(df_equal: pd.DataFrame):
-    X_train, X_test = train_test_split(df_equal, test_size=0.3, random_state=12)
+    # df_small = df_equal.sample(200000, random_state=42)
+    # print("Shape df_small:", df_small.shape)
+    # X_train, X_test = train_test_split(df_small, test_size=0.3, random_state=42)
+    X_train, X_test = train_test_split(df_equal, test_size=0.3, random_state=42)
 
     y_train = X_train.pop("Label")
     y_test = X_test.pop("Label")
 
     model = RandomForestClassifier(
-        n_estimators=75,
-        criterion="gini",
+        n_estimators=300,
+        criterion="entropy",
         max_depth=20,
-        min_samples_split=2,
+        min_samples_split=5,
         min_samples_leaf=1,
         min_weight_fraction_leaf=0.0,
         max_features="sqrt",
@@ -61,28 +62,86 @@ def train_model(df_equal: pd.DataFrame):
         bootstrap=True,
         oob_score=False,
         n_jobs=-1,
-        random_state=None,
+        random_state=42,
         verbose=0,
         warm_start=False,
         ccp_alpha=0.0,
         max_samples=None,
     )
-
-    # hyperparameters = {"n_estimators": [75]}
-
-    # clf = GridSearchCV(
-    #     estimator=model,
-    #     param_grid=hyperparameters,
-    #     cv=2,
-    #     verbose=1,
-    #     n_jobs=-1,
-    # )
-
     print("\nTraining...")
-    # clf.fit(X=X_train, y=y_train)
-    model.fit(X=X_train, y=y_train)
+    model.fit(X_train, y_train)
 
     return model, X_test, X_train, y_test
+
+
+def find_best_hyp(X_train, y_train):
+
+    # RandomSearchCV
+    model = RandomForestClassifier(
+        n_estimators=50,
+        criterion="gini",
+        max_depth=20,
+        min_samples_split=2,
+        min_samples_leaf=2,
+        min_weight_fraction_leaf=0.0,
+        max_features="sqrt",
+        max_leaf_nodes=None,
+        min_impurity_decrease=0.0,
+        bootstrap=True,
+        oob_score=False,
+        n_jobs=-1,
+        random_state=42,
+        verbose=0,
+        warm_start=False,
+        ccp_alpha=0.0,
+        max_samples=None,
+    )
+    param_dist = {
+        "n_estimators": [100, 200, 300],
+        "max_depth": [10, 20, 30],
+        "min_samples_split": [2, 5],
+        "min_samples_leaf": [1, 2, 4],
+        "criterion": ["gini", "entropy"],
+    }
+    random_search = RandomizedSearchCV(
+        estimator=model,
+        param_distributions=param_dist,
+        n_iter=10,
+        cv=3,
+        verbose=1,
+        random_state=42,
+        n_jobs=-1,
+        scoring="f1",
+    )
+    random_search.fit(X_train, y_train)
+    # Evaluation
+    print("Best params:\n", random_search.best_params_)
+    print("Best score:\n", random_search.best_score_)
+    best_model = random_search.best_estimator_
+    results = pd.DataFrame(random_search.cv_results_)
+    print(
+        results[
+            [
+                "mean_test_score",
+                "param_n_estimators",
+                "param_max_depth",
+                "param_min_samples_leaf",
+            ]
+        ].sort_values(by="mean_test_score", ascending=False)
+    )
+
+    # GridSearchCV
+    hyperparameters = {"n_estimators": [75, 100, 150, 200]}
+    clf = GridSearchCV(
+        estimator=model,
+        param_grid=hyperparameters,
+        cv=2,
+        verbose=1,
+        n_jobs=-1,
+    )
+    clf.fit(X=X_train, y=y_train)
+    print("\nTraining...")
+    best_model.fit(X=X_train, y=y_train)
 
 
 def evaluate(model, X_test, y_test, threshold=0.43):
@@ -132,11 +191,11 @@ def evaluate(model, X_test, y_test, threshold=0.43):
 
 
 def feature_importance(model, X_test, y_test, top_n=10):
-    X_small = X_test.sample(50000, random_state=42)
+    X_small = X_test.sample(500000, random_state=42)
     y_small = y_test[X_small.index]
 
     result = permutation_importance(
-        model, X_small, y_small, n_repeats=2, random_state=42, n_jobs=-1
+        model, X_small, y_small, n_repeats=2, random_state=42, n_jobs=1
     )
 
     fi = pd.Series(result.importances_mean, index=X_test.columns).sort_values(
@@ -165,11 +224,11 @@ def main():
 
     df_equal = load_and_prepare()
 
-    model, X_test, X_train, y_test = train_model(df_equal)
+    best_model, X_test, X_train, y_test = train_model(df_equal)
 
-    evaluate(model, X_test, y_test)
+    evaluate(best_model, X_test, y_test)
 
-    feature_importance(model, X_test, y_test)
+    feature_importance(best_model, X_test, y_test)
 
 
 if __name__ == "__main__":
