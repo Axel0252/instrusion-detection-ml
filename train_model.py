a29210fe146fed11810fae66dac1f28545197246
Axel0252 <tafuro561@gmail.com>
2026-05-14 12:34:26 +0200

    Added some small cli command & ROC curve

--- train_model.py
+++ train_model.py
@@ -1,11 +1,10 @@
+import argparse
 import os
 
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.inspection import permutation_importance
 from sklearn.metrics import (
     accuracy_score,
     average_precision_score,
@@ -15,14 +14,26 @@ from sklearn.metrics import (
     precision_score,
     recall_score,
     roc_auc_score,
+    roc_curve,
 )
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, train_test_split
 
 import misc
 
 # Configs
-# DROP_COLUMNS = ["Timestamp", "Src IP", "Dst IP", "Flow ID"] not dropped in the latest experiments
+# DROP_COLUMNS = ["Src IP", "Dst IP", "Flow ID"] not dropped in the latest experiments
 CLEAN_DATASET = "cic_ids2018_cleaned.csv"
+
+# Argument parser
+parser = argparse.ArgumentParser(prog="train_model")
+parser.add_argument(
+    "-y",
+    "--hyp",
+    type=str,
+    default=None,
+    help="(Optional) Set the specified class to choose the best hyperparameters (accepts only 'gs' or 'rs')",
+)
+args = parser.parse_args()
 
 
 def load_and_prepare():
@@ -41,112 +52,109 @@ def load_and_prepare():
 
 
 def train_model(df_equal: pd.DataFrame):
-    # df_small = df_equal.sample(200000, random_state=42)
-    # print("Shape df_small:", df_small.shape)
-    # X_train, X_test = train_test_split(df_small, test_size=0.3, random_state=42)
-    X_train, X_test = train_test_split(df_equal, test_size=0.3, random_state=42)
+    if args.hyp is not None:
+        df_small = df_equal.sample(200000, random_state=42)
+        print("Shape df_small:", df_small.shape)
+        X_train, X_test = train_test_split(df_small, test_size=0.3, random_state=42)
+        y_train = X_train.pop("Label")
+        y_test = X_test.pop("Label")
 
-    y_train = X_train.pop("Label")
-    y_test = X_test.pop("Label")
+        model = find_best_hyp(X_train, y_train, args.hyp)
+    else:
+        X_train, X_test = train_test_split(df_equal, test_size=0.3, random_state=42)
+        y_train = X_train.pop("Label")
+        y_test = X_test.pop("Label")
 
-    model = RandomForestClassifier(
-        n_estimators=300,
-        criterion="entropy",
-        max_depth=20,
-        min_samples_split=5,
-        min_samples_leaf=1,
-        min_weight_fraction_leaf=0.0,
-        max_features="sqrt",
-        max_leaf_nodes=None,
-        min_impurity_decrease=0.0,
-        bootstrap=True,
-        oob_score=False,
-        n_jobs=-1,
-        random_state=42,
-        verbose=0,
-        warm_start=False,
-        ccp_alpha=0.0,
-        max_samples=None,
-    )
+        model = RandomForestClassifier(
+            n_estimators=100,
+            min_samples_split=20,
+            min_samples_leaf=1,
+            max_features="sqrt",
+            max_depth=None,
+            n_jobs=-1,
+        )
+
     print("\nTraining...")
     model.fit(X_train, y_train)
 
     return model, X_test, X_train, y_test
 
 
-def find_best_hyp(X_train, y_train):
+def find_best_hyp(X_train, y_train, alg):
+    model = RandomForestClassifier(random_state=42, n_jobs=-1)
 
-    # RandomSearchCV
-    model = RandomForestClassifier(
-        n_estimators=50,
-        criterion="gini",
-        max_depth=20,
-        min_samples_split=2,
-        min_samples_leaf=2,
-        min_weight_fraction_leaf=0.0,
-        max_features="sqrt",
-        max_leaf_nodes=None,
-        min_impurity_decrease=0.0,
-        bootstrap=True,
-        oob_score=False,
-        n_jobs=-1,
-        random_state=42,
-        verbose=0,
-        warm_start=False,
-        ccp_alpha=0.0,
-        max_samples=None,
-    )
-    param_dist = {
-        "n_estimators": [100, 200, 300],
-        "max_depth": [10, 20, 30],
-        "min_samples_split": [2, 5],
-        "min_samples_leaf": [1, 2, 4],
-        "criterion": ["gini", "entropy"],
-    }
-    random_search = RandomizedSearchCV(
-        estimator=model,
-        param_distributions=param_dist,
-        n_iter=10,
-        cv=3,
-        verbose=1,
-        random_state=42,
-        n_jobs=-1,
-        scoring="f1",
-    )
-    random_search.fit(X_train, y_train)
-    # Evaluation
-    print("Best params:\n", random_search.best_params_)
-    print("Best score:\n", random_search.best_score_)
-    best_model = random_search.best_estimator_
-    results = pd.DataFrame(random_search.cv_results_)
+    if alg == "rs":
+        # RandomizedSearchCV
+        # Best params: {'n_estimators': 100, 'min_samples_split': 20, 'min_samples_leaf': 1, 'max_features': 'sqrt', 'max_depth': None}
+        # Best score: 0.9823069477615828
+        hyperparameters = {
+            "n_estimators": [100, 200, 300],
+            "max_depth": [None, 15, 25, 40],
+            "min_samples_split": [2, 5, 10, 20],
+            "min_samples_leaf": [1, 2, 4, 8],
+            "max_features": ["sqrt", "log2"],
+        }
+        cv = RandomizedSearchCV(
+            estimator=model,
+            param_distributions=hyperparameters,
+            n_iter=30,
+            cv=3,
+            verbose=1,
+            random_state=42,
+            n_jobs=-1,
+            scoring="f1",
+        )
+        cv.fit(X_train, y_train)
+    elif alg == "gs":
+        # GridSearchCV
+        param_grid = {
+            "n_estimators": [300],
+            "max_depth": [None, 30],
+            "min_samples_split": [2, 5],
+            "min_samples_leaf": [1, 2],
+            "max_features": ["sqrt"],
+        }
+        cv = GridSearchCV(
+            estimator=model,
+            param_grid=param_grid,
+            cv=3,
+            verbose=1,
+            n_jobs=-1,
+            scoring="f1",
+        )
+        cv.fit(X=X_train, y=y_train)
+    else:
+        print("Wrong hyp algorithm selected.")
+        return -1
+
+    # Results
+    print("Best params:\n", cv.best_params_)
+    print("Best score:\n", cv.best_score_)
+    best_model = cv.best_estimator_
+    results = pd.DataFrame(cv.cv_results_)
     print(
         results[
             [
                 "mean_test_score",
                 "param_n_estimators",
+                "param_min_samples_split",
                 "param_max_depth",
                 "param_min_samples_leaf",
+                "param_max_features",
             ]
         ].sort_values(by="mean_test_score", ascending=False)
     )
-
-    # GridSearchCV
-    hyperparameters = {"n_estimators": [75, 100, 150, 200]}
-    clf = GridSearchCV(
-        estimator=model,
-        param_grid=hyperparameters,
-        cv=2,
-        verbose=1,
-        n_jobs=-1,
-    )
-    clf.fit(X=X_train, y=y_train)
     print("\nTraining...")
     best_model.fit(X=X_train, y=y_train)
 
+    return best_model
 
-def evaluate(model, X_test, y_test, threshold=0.43):
+
+def evaluate(model, X_test, y_test):
     y_proba = model.predict_proba(X_test)[:, 1]
-    y_pred = (y_proba >= threshold).astype(int)
+
+    # Default threshold
+    y_pred = (y_proba >= 0.5).astype(int)
 
     tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
 
@@ -168,6 +176,24 @@ def evaluate(model, X_test, y_test, threshold=0.43):
     print("\n=== CLASSIFICATION REPORT ===")
     print(classification_report(y_test, y_pred))
 
+    # ROC CURVE
+    fpr, tpr, thresholds = roc_curve(y_test, y_proba)
+    roc_auc = roc_auc_score(y_test, y_proba)
+
+    plt.figure(figsize=(7, 6))
+
+    plt.plot(fpr, tpr, label=f"ROC curve (AUC = {roc_auc:.4f})")
+    plt.plot([0, 1], [0, 1], linestyle="--", color="gray")
+
+    plt.title("ROC Curve")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.legend()
+    plt.tight_layout()
+
+    plt.savefig("imgs/roc_curve.png", dpi=300)
+
+    # CONFUSION MATRIX
     cm = [[tn, fp], [fn, tp]]
 
     plt.figure(figsize=(6, 5))
@@ -191,26 +217,34 @@ def evaluate(model, X_test, y_test, threshold=0.43):
 
 
 def feature_importance(model, X_test, y_test, top_n=10):
-    X_small = X_test.sample(500000, random_state=42)
-    y_small = y_test[X_small.index]
+    if args.hyp is None:
+        X_small = X_test.sample(500000, random_state=42)
+        y_small = y_test[X_small.index]
 
-    result = permutation_importance(
-        model, X_small, y_small, n_repeats=2, random_state=42, n_jobs=1
-    )
+    # result = permutation_importance(
+    #     model, X_test, y_test, n_repeats=2, random_state=42, n_jobs=1
+    # )
 
-    fi = pd.Series(result.importances_mean, index=X_test.columns).sort_values(
-        ascending=False
-    )
+    # fi = pd.Series(result.importances_mean, index=X_test.columns).sort_values(
+    #     ascending=False
+    # )
 
-    print(fi.head(top_n))
+    fn = X_test.columns
+    fi = pd.DataFrame({"feature": fn, "importance": model.feature_importances_})
+
+    fi = fi.sort_values(by="importance", ascending=False)
+    top10 = fi.head(10)
+
+    print(fi.head(10))
 
     plt.figure(figsize=(10, 6))
 
-    fi.head(top_n).sort_values().plot(kind="barh")
+    plt.barh(top10["feature"], top10["importance"])
 
-    plt.xlabel("Permutation Importance")
-    plt.ylabel("Feature")
-    plt.title("Top Feature Importance")
+    plt.gca().invert_yaxis()
+
+    plt.xlabel("Importance")
+    plt.title("Top 10 Feature Importances")
 
     plt.tight_layout()
     plt.savefig("imgs/feature_importance.png", dpi=300)
